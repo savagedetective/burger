6dc1f0db7e49d27bd6ce02a1adf71ef168c4b90f
Ritchie <ritch.g@gmail.com>
2019-03-26 15:24:11 -0700

    adds "final" functionality

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,114 @@
 //grab SQL connection
 var connection = require("../config/connection.js");
 
+//prints question mark for queryString for each burger found in db
+function printQuestionMarks(num) {
+
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+
+    var arr = [];
+
+    for (var key in ob) {
+
+        var value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
 //holds our ORM and included functions
 var orm = {
-    selectAll: function(tableInput, cb) {
+    selectAll: function (tableInput, cb) {
         console.log("orm/selectAll hit. Loading burgers.");
         var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    insertOne: function (table, cols, vals, cb) {
+        console.log("orm/insertOne hit. Creating burger.");
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    updateOne: function (table, objColVals, condition, cb) {
+
+        console.log("this will update a burger.");
+
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
         connection.query(queryString, function(err, result) {
           if (err) {
             throw err;
           }
+    
           cb(result);
         });
+
     },
 
-    insertOne: function() {
-        console.log("orm/insertOne hit. Creating burger");
-    },
+    delete: function(table, condition, cb) {
 
-    updateOne: function() {
-        console.log("this will update a burger");
-    }
+        console.log("orm/delete hit. Deleting Burger.")
+
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
+
 };
 
 //export
--- public/js/app.js
+++ public/js/app.js
@@ -17,10 +17,53 @@ $(document).ready(function() {
             data: newBurger
         })
             .then(function(){
-                console.log("Burger Created.")
+                console.log("Burger Created.");
+                $("#burger-form-input").val("");
                 location.reload();
             });
 
+    });
+
+    //logic to delete burger
+    $(".delete-burger").on("click", function(event) {
+
+        var id = $(this).attr("data-id");
+
+        console.log("Delete burger button clicked - " + id);
+
+        $.ajax("/api/burgers/" + id, {
+            type:"DELETE"
+        })
+            .then(
+                function() {
+                    console.log("burger deleted", id);
+                    location.reload();
+                }
+            );
+    });
+
+    //logic to change devour state
+
+    $(".change-devoured").on("click", function(event) {
+
+        var id = $(this).attr("data-id");
+        var devState = $(this).attr("not-devoured");
+
+        console.log("id is " + id + ", devState is " + devState);
+
+        var newDevoured = {
+            devoured: devState
+        };
+
+        $.ajax("/api/burgers/" + id, {
+            type:"PUT",
+            data: newDevoured
+        })
+            .then(function() {
+                console.log("changed devoured to", newDevoured);
+                location.reload();
+            });
+        
     });
 
 });
